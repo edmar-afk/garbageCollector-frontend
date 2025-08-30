95560a880f43041aa4af268ccf47bb67e593cc83
edmar-afk <jaywrsnp6@gmail.com>
2025-08-30 08:03:36 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
-import { defineConfig } from "vite"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	plugins: [
 		react(),
-		tailwindcss()
+		tailwindcss(),
+		VitePWA({
+			registerType: "prompt",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			manifest: {
+				name: "Cooking App",
+				short_name: "Cooking App",
+				description: "A Progressive App for Cooking Recipes",
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#0f172a",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
 	],
-})
+});
